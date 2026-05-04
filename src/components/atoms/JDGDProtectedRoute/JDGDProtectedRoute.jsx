1daf1537e009f24af1e909e2e2ac5b14c90fd373
jehison-Gomez <jehisondavidgomezdelgado@gmail.com>
2026-05-04 10:18:10 -0500

    Configuracion para el ingreso por rol

--- src/components/atoms/JDGDProtectedRoute/JDGDProtectedRoute.jsx
+++ src/components/atoms/JDGDProtectedRoute/JDGDProtectedRoute.jsx
@@ -1,9 +1,17 @@
 import { Navigate } from 'react-router-dom'
 import { JDGDUseAuth } from '../../../context/JDGDAuthContext'
 
-const JDGDProtectedRoute = ({ children }) => {
+const JDGDProtectedRoute = ({ children, roles }) => {
   const { JDGDUser } = JDGDUseAuth()
-  return JDGDUser ? children : <Navigate to="/login" replace />
+
+  if (!JDGDUser) return <Navigate to="/login" replace />
+
+  if (roles && !roles.includes(JDGDUser.rol)) {
+    if (JDGDUser.rol === 'cliente') return <Navigate to="/cliente/prestamos" replace />
+    return <Navigate to="/dashboard" replace />
+  }
+
+  return children
 }
 
 export default JDGDProtectedRoute
